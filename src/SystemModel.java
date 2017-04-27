5ab236184b40141e73076ea4cd2bf0ced4509aba
Elin Ljunggren <elin.l.ljunggren@gmail.com>
2017-04-27 16:06:38 +0200

    Added model class for a system

---
+++ src/SystemModel.java
@@ -0,0 +1,43 @@
+import java.util.List;
+import java.util.Map;
+
+public class SystemModel {
+
+    private Junction rootNode;
+    private List<AV> AVs;
+
+    // The key is an AV ID and the value is the list of InletClusters that belong to the AV
+    private Map<String,List<InletCluster>> avClusters;
+
+    public SystemModel(Junction rootNode, List<AV> AVs, Map<String,List<InletCluster>> avClusters) {
+        this.rootNode = rootNode;
+        this.AVs = AVs;
+        this.avClusters = avClusters;
+
+    }
+
+
+    public Junction getRootNode() {
+        return rootNode;
+    }
+
+    public void setRootNode(Junction rootNode) {
+        this.rootNode = rootNode;
+    }
+
+    public List<AV> getAVs() {
+        return AVs;
+    }
+
+    public void setAVs(List<AV> AVs) {
+        this.AVs = AVs;
+    }
+
+    public Map<String, List<InletCluster>> getAvClusters() {
+        return avClusters;
+    }
+
+    public void setAvClusters(Map<String, List<InletCluster>> avClusters) {
+        this.avClusters = avClusters;
+    }
+}
